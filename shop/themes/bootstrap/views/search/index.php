2f2d74064dba8512c6cddd0b5b005d84611b4510
phisic <murod9@gmail.com>
2013-04-09 12:04:07 +0500

    product detail in listing

--- shop/themes/bootstrap/views/search/index.php
+++ shop/themes/bootstrap/views/search/index.php
@@ -1,17 +1,17 @@
 <h3>Search Result</h3>
-<div class="items">
-<ul class="thumbnails">
 <?php foreach ($items as $item){ ?>
-<li class="span4">  
-<div class="thumbnail">
-  <img src="<?=isset($item['MediumImage']['URL']) ?str_replace("._SL160_.","._AA160_.",$item['MediumImage']['URL']) :  Yii::app()->createUrl('images') . '/none.jpg';?>" alt="product 2">
-  <div class="caption">  
-    <h5>Product detail</h5>  
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  
-    <p><a href="#" class="btn btn-success">Buy</a> <a href="#" class="btn btn-warning">Try</a></p>  
-  </div>  
-</div>  
-</li>
+<div class="row">
+  <div class="span2"><img src="<?=isset($item['MediumImage']['URL']) ?str_replace("._SL160_.","._AA160_.",$item['MediumImage']['URL']) :  Yii::app()->createUrl('images') . '/none.jpg';?>" alt="product 2"></div>
+  <div class="span8">
+      <h4><a href=''><?=$item['ItemAttributes']['Title']?></a> <span class='text-warning'style='font-size:12px;'>by <?=$item['ItemAttributes']['Brand']?></span></h4>
+      <h6><ul>
+      <?php foreach($item['ItemAttributes']['Feature'] as $attr){
+          echo '<li>'.$attr.'</li>';
+      }?>
+              </ul>
+      </h6>
+  </div>
+  <div class="span2"><a href="#" class="btn btn-success">Buy</a></div>
+  </div>
 <?php } ?>
-</ul></div>  
 <? $this->widget('ext.bootstrap.widgets.TbPager', array('htmlOptions'=>array('class'=>'pager'),'pages' => $pages)); ?>
