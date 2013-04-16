196f5309876bb71e6938634e8c1a7e116916d4c5
phisic <murod9@gmail.com>
2013-04-17 00:39:49 +0500

    carousel added

--- shop/protected/extensions/bootstrap/widgets/TbCarousel.php
+++ shop/protected/extensions/bootstrap/widgets/TbCarousel.php
@@ -149,37 +149,7 @@ class TbCarousel extends CWidget
 
 			echo CHtml::openTag('div', $item['itemOptions']);
 
-			if (isset($item['image']))
-			{
-				if (!isset($item['alt']))
-					$item['alt'] = '';
-
-				if (!isset($item['imageOptions']))
-					$item['imageOptions'] = array();
-
-				echo CHtml::image($item['image'], $item['alt'], $item['imageOptions']);
-			}
-
-			if (!empty($item['caption']) && (isset($item['label']) || isset($item['caption'])))
-			{
-				if (!isset($item['captionOptions']))
-					$item['captionOptions'] = array();
-
-				if (isset($item['captionOptions']['class']))
-					$item['captionOptions']['class'] .= ' carousel-caption';
-				else
-					$item['captionOptions']['class'] = 'carousel-caption';
-
-				echo CHtml::openTag('div', $item['captionOptions']);
-
-				if (isset($item['label']))
-					echo '<h4>'.$item['label'].'</h4>';
-
-				if (isset($item['caption']))
-					echo '<p>'.$item['caption'].'</p>';
-
-				echo '</div>';
-			}
+			echo $item['text'];
 			echo '</div>';
 		}
 	}
--- shop/themes/bootstrap/views/site/index.php
+++ shop/themes/bootstrap/views/site/index.php
@@ -1,1 +1,35 @@
-../search/index.php
+<?php
+
+$list = array();
+foreach ($items as $i) {
+    $newPrice = Yii::app()->amazon->getNewPrice($i);
+    $usedPrice = Yii::app()->amazon->getUsedPrice($i);
+    $p='';
+    if (isset($i['ItemAttributes']['ListPrice']['Amount']))
+        $p = '<s class="muted" style="font-size:12px;">' . Yii::app()->amazon->formatUSD($i['ItemAttributes']['ListPrice']['Amount']) . '</s>';
+    if ($newPrice)
+        $p .=' <a href="" class="text-error" style="font-size:26px;"><strong>' . Yii::app()->amazon->formatUSD($newPrice) . '</strong></a> new';
+    if ($newPrice && $usedPrice)
+        $p .= ' <span style="font-size:16px;"> & </span> ';
+    if ($usedPrice)
+        $p .= ' <a href="" class="text-error" style="font-size:26px;"><strong>' . Yii::app()->amazon->formatUSD($usedPrice) . '</strong></a> used';
+    $img = isset($i['LargeImage']['URL']) ? str_replace(".jpg", "._AA500_.jpg", $i['LargeImage']['URL']) : Yii::app()->createUrl('images') . '/none.jpg';
+    $list[] = array(
+        'text' => '
+            <div class="row">
+                <div class="span6"><img src="' . $img . '"></div>
+                <div class="span6" style="padding-top:40px;">
+                 <h3><a href="">' . $i['ItemAttributes']['Title'] . '</a> <span class="text-warning"style="font-size:12px;">by ' . $i['ItemAttributes']['Brand'] . '</span></h3>  
+                 <h4>
+                    <ul>
+                        <li>' . join('</li><li>', $i['ItemAttributes']['Feature']) . '</li>
+                    </ul>
+                 </h4>
+                 <h3>' . $p . '</h3>
+                </div>
+           </div>');
+}
+$this->widget('bootstrap.widgets.TbCarousel', array(
+    'items' => $list
+));
+?>
