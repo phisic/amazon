a430a3d64aacf78cbda924078f0057b2b4e0c368
phisic <murod9@gmail.com>
2013-04-25 23:36:31 +0500

    user default page

--- shop/protected/modules/user/controllers/DefaultController.php
+++ shop/protected/modules/user/controllers/DefaultController.php
@@ -8,18 +8,7 @@ class DefaultController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('User', array(
-			'criteria'=>array(
-		        'condition'=>'status>'.User::STATUS_BANNED,
-		    ),
-			'pagination'=>array(
-				'pageSize'=>Yii::app()->controller->module->user_page_size,
-			),
-		));
-
-		$this->render('/user/index',array(
-			'dataProvider'=>$dataProvider,
-		));
+		$this->forward('/user/profile');
 	}
 
 }
