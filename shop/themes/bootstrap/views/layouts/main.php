cc9b46d54f437a6cf18d49224e4ef8d8259cdf37
root <root@mail.maverickuz.com>
2013-05-14 07:25:24 +0200

    google analytics

--- shop/themes/bootstrap/views/layouts/main.php
+++ shop/themes/bootstrap/views/layouts/main.php
@@ -13,6 +13,20 @@
         <script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl; ?>/js/<?=Yii::app()->user->getIsGuest() ? 'watchGuest.js' : 'watchUser.js'?>"></script>
     </head>
     <body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-40874781-1']);
+  _gaq.push(['_setDomainName', 'laptoptop7.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
         <div class="navbar navbar-fixed-top">
             <div class="navbar-inner">
                 <div class="container">
@@ -93,15 +107,5 @@
             echo '<script type="text/javascript">var watchUrl = "'.Yii::app()->createUrl('watch/index').'"</script>';
         } 
         ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40875729-1', 'laptoptop7.com');
-  ga('send', 'pageview');
-
-</script>
-    </body>
+</body>
 </html>
