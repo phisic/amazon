254fdb77a882d7b03f41892d20ebd4baf551fcf3
phisic <murod9@gmail.com>
2013-05-27 12:01:55 +0500

    microdata snippets

--- shop/index-test.php
+++ shop/index-test.php
@@ -1,2 +1,48 @@
-<?php
-phpinfo();
+<div itemscope itemtype="http://schema.org/Product">
+    <span itemprop="description"> notebook computer.<br><br>
+        <span style="font-weight: bold; font-size: 12px" itemprop="name">MacBook Pro</span>
+        <div style="display: inline; font-family: Arial, Tahoma;" itemprop="offers" itemscope itemtype="http://schema.org/AggregateOffer">  
+            <span style="font-weight: bold; font-size: 12px">Lowest Price:</span>  
+            <span itemprop="lowPrice">$1,112.78</span>  
+            <span style="font-weight: bold; font-size: 12px">Highest Price:</span>  
+            <span itemprop="highPrice">$2,699.99</span>  
+        </div>    
+        <div style="display: inline; font-family: Arial, Tahoma; font-weight: bold;" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">  
+            <span style="font-weight: bold; font-size: 12px">Average User Rating:</span>  
+            <span style="font-weight: normal; font-size: 12px">  
+                <span itemprop="ratingValue">4.6</span> out of 
+                <span itemprop="bestRating">5</span> based on 
+                <span itemprop="ratingCount">177</span> customer reviews  
+            </span>  
+        </div>  
+</div>
+
+<div itemscope itemtype="http://data-vocabulary.org/Product">
+  <span itemprop="brand">ACME</span> <span itemprop="name">Executive
+    Anvil</span>
+  <img itemprop="image" src="anvil_executive.jpg" />
+
+  <span itemprop="description">Sleeker than ACME's Classic Anvil, the 
+    Executive Anvil is perfect for the business traveler
+    looking for something to drop from a height.
+  </span>
+  Category: <span itemprop="category" content="Hardware > Tools > Anvils">Anvils</span>
+  Product #: <span itemprop="identifier" content="mpn:925872">
+    925872</span>
+  <span itemprop="review" itemscope itemtype="http://data-vocabulary.org/Review-aggregate">
+    <span itemprop="rating">4.4</span> stars, based on <span itemprop="count">89
+      </span> reviews
+  </span>
+
+  <span itemprop="offerDetails" itemscope itemtype="http://data-vocabulary.org/Offer">
+    Regular price: $179.99
+    <meta itemprop="currency" content="USD" />
+    $<span itemprop="price">119.99</span>
+    (Sale ends <time itemprop="priceValidUntil" datetime="2020-11-05">
+      5 November!</time>)
+    Available from: <span itemprop="seller">Executive Objects</span>
+    Condition: <span itemprop="condition" content="used">Previously owned, 
+      in excellent condition</span>
+    <span itemprop="availability" content="in_stock">In stock! Order now!</span>
+  </span>
+</div>
