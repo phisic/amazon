348a288c312eb9753917006f3409cf88157e53f7
phisic <murod9@gmail.com>
2013-05-29 00:42:01 +0500

    seo

--- shop/protected/components/WebApplication.php
+++ shop/protected/components/WebApplication.php
@@ -13,10 +13,16 @@ class WebApplication extends CWebApplication {
 
     public function createSeoUrl($route,$string) 
     {
+        if(strlen($string)>100)
+            $string = substr ($string, 0, 100);
+        
         $string = preg_replace("/[^a-zA-Z0-9 -]/", "", $string);
         $string = strtolower($string);
+        $pos = strrpos($route, '/');
+        $param = substr($route, $pos+1);
+        $route = substr($route,0,$pos+1);
         $string = str_replace(" ", '-', $string);
-        return $this->createUrl($route.'-'.$string);
+        return $this->createUrl($route.$string.'-'.$param);
     }
 
 }
