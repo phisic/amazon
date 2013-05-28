bfe9a03bd644bc6384345755c3204cce93c1079e
phisic <murod9@gmail.com>
2013-05-29 00:54:42 +0500

    seo

--- shop/protected/components/WebApplication.php
+++ shop/protected/components/WebApplication.php
@@ -11,18 +11,15 @@ class WebApplication extends CWebApplication {
         Yii::$classMap += require(Yii::app()->basePath . '/config/classmap.php');
     }
 
-    public function createSeoUrl($route,$string) 
-    {
-        if(strlen($string)>100)
-            $string = substr ($string, 0, 100);
-        
-        $string = preg_replace("/[^a-zA-Z0-9 -]/", "", $string);
-        $string = strtolower($string);
+    public function createSeoUrl($route, $string) {
+        if (strlen($string) > 100)
+            $string = substr($string, 0, 100);
+        $string = strtolower(preg_replace(array('/[^a-z0-9\- ]/i', '/[ \-]+/'), array('', '-'), $string));
+        $string = trim($string,'-');
         $pos = strrpos($route, '/');
-        $param = substr($route, $pos+1);
-        $route = substr($route,0,$pos+1);
-        $string = str_replace(" ", '-', $string);
-        return $this->createUrl($route.$string.'-'.$param);
+        $param = substr($route, $pos + 1);
+        $route = substr($route, 0, $pos + 1);
+        return $this->createUrl($route . $string . '-' . $param);
     }
 
 }
