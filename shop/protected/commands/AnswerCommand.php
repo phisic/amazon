fc5f897c9718fa740cc1d7edaa479671140a0647
phisic <murod9@gmail.com>
2013-07-11 11:12:16 +0500

    replace

--- shop/protected/commands/AnswerCommand.php
+++ shop/protected/commands/AnswerCommand.php
@@ -51,7 +51,7 @@ class AnswerCommand extends CConsoleCommand {
                                 Yii::app()->db->getCommandBuilder()->createInsertCommand('answer', array('QId' => $qid, 'Text' => $answer))->execute();
                             }
                         }
-                        @Yii::app()->db->getCommandBuilder()->createInsertCommand('listing2question', array('QId' => $qid, 'ASIN' => $r['ASIN']))->execute();
+                        @Yii::app()->db->getCommandBuilder()->createSqlCommand('Replace into listing2question (QId,ASIN) Values(:qid,:asin)', array(':qid' => $qid, ':asin' => $r['ASIN']))->execute();
                     }
                     $c = new CDbCriteria();
                     $c->compare('ASIN', $r['ASIN']);
